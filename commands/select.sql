9226439c55dd8311a4d8637c21ec96edff97ec48
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-05-01 11:54:20 -0300

    feat: add description for select commands

--- commands/select.sql
+++ commands/select.sql
@@ -1,35 +1,43 @@
+-- Basic
 select u.first_name, u.last_name from users as u;
 
+-- Where condition
 select u.first_name, u.last_name from users as u 
 where u.updated_at  < now() and id > 1;
 
 select u.first_name, u.last_name from users as u 
 where u.updated_at  < now() or id > 1;
 
+-- Between condition
 select * from users
 where created_at between '2020-06-12 17:38:52' and '2020-09-04 19:06:55';
 
+-- In Condition
 select u.id, u.first_name, u.last_name  from users u
 where id in (23, 62, 18)
 
+-- Like Condition
 select u.id, u.first_name, u.last_name  from users u
 where first_name like '%ar%';
 
 select u.id, u.first_name, u.last_name  from users u
 where first_name like '______';
 
+-- Order By Condition
 select u.id, u.first_name, u.last_name, u.created_at  from users u
 where id > 33 order by created_at asc;
 
 select u.id, u.first_name, u.last_name, u.created_at  from users u
 where id > 33 order by first_name asc, id desc;
 
+-- Limit and Offset Condition
 select u.id, u.first_name, u.last_name, u.created_at  from users u
 where id > 33 order by id asc limit 3 offset 6;
 
 select u.id, u.first_name, u.last_name, u.created_at  from users u
 where id > 33 order by id asc limit 10, 5;
 
+-- Insert with Select
 insert into profiles 
 (bio, description, user_id)
 select 'bio', 'description', id 
