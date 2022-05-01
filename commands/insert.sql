61ec8e406c25570fb6e29f947b6e47d434706d3b
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-05-01 13:53:00 -0300

    feat: Insert with multiple joins

--- commands/insert.sql
+++ commands/insert.sql
@@ -18,3 +18,14 @@ select
 id, 
 (select id from roles order by rand() limit 1) as qualquer 
 from users order by rand() limit 5;
+
+-- Insert with multiple joins
+select u.id as uid, u.first_name, p.bio, r.name
+from users as u
+left join profiles as p 
+on u.id = p.user_id 
+inner join user_roles as ur 
+on u.id = ur.user_id 
+inner join roles as r 
+on ur.role_id = r.id
+order by uid;
