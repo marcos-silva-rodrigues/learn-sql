1e4528f2546192f6d50da3ea3420a139f34a599b
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-05-01 14:12:49 -0300

    feat: update e delete with joins

--- commands/delete.sql
+++ commands/delete.sql
@@ -1,1 +1,7 @@
 delete from users where id = 39;
+
+-- Delete with Join
+delete p, u from users as u
+left join profiles as p
+on p.user_id  = u.id
+where u.first_name = "Katelyn";
--- commands/update.sql
+++ commands/update.sql
@@ -1,3 +1,10 @@
 update users 
 set first_name = 'Luiz', last_name = 'Miranda'
 where id = 7;
+
+-- Update with join
+update users as u
+join profiles as p
+on p.user_id  = u.id
+set p.bio = concat(p.bio, ' atualizado')
+where u.first_name = "Katelyn";
