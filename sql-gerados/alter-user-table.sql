fe895491df40306e4f4addcee83d86e1cceee2d0
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-05-01 12:48:30 -0300

    feat: add salary column

--- sql-gerados/alter-user-table.sql
+++ sql-gerados/alter-user-table.sql
@@ -1,3 +1,4 @@
 ALTER TABLE learnsql.users ADD created_at DATETIME DEFAULT NOW() NOT NULL;
 ALTER TABLE learnsql.users ADD updated_at DATETIME DEFAULT now() on update now() NOT NULL;
+ALTER TABLE learnsql.users ADD salary DECIMAL(15, 2) NULL;
 
